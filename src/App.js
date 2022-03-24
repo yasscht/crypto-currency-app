2cadc2ab0fd5b18af6cf3ef5794ee1de54592796
yassine.chtiaa@etu.uae.ac.ma <yassine.chtiaa@etu.uae.ac.ma>
2022-03-24 11:40:47 +0100

    add the navbar layout to the project

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
+import { Switch, Link, Route } from "react-router-dom";
+import { Layout, Typography, Space } from "antd";
+import { NavBar } from "./components";
+import "./App.css";
 function App() {
-  return <div className="App">app</div>;
+  return (
+    <>
+      <div className="app">
+        <div className="navbar">
+          <NavBar />
+        </div>
+        <div className="main"></div>
+
+        <div className="footer"></div>
+      </div>
+    </>
+  );
 }
 
 export default App;
