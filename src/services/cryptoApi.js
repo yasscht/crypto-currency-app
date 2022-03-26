fb369aad31b7a3cb4dba398f32932735812868da
yassine.chtiaa@etu.uae.ac.ma <yassine.chtiaa@etu.uae.ac.ma>
2022-03-26 13:54:14 +0100

    creatng of the getCoinDetails service and retriving the coinId

--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -12,6 +12,9 @@ export const cryptoApi = createApi({
     getCryptos: builder.query({
       query: (count) => createRequest(`/coins?limit=${count}`),
     }),
+    getCryptoDetails: builder.query({
+      query: (id) => createRequest(`/coin/${id}`),
+    }),
   }),
 });
-export const { useGetCryptosQuery } = cryptoApi;
+export const { useGetCryptosQuery, useGetCryptoDetailsQuery } = cryptoApi;
