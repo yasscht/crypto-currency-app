ed119763d1992983c50c4ccc2d61e267ab814272
yassine.chtiaa@etu.uae.ac.ma <yassine.chtiaa@etu.uae.ac.ma>
2022-03-29 11:43:36 +0000

    set up the exchanges page

--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -1,7 +1,7 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 const cryptoApiHeaders = {
   "X-RapidAPI-Host": "coinranking1.p.rapidapi.com",
-  "X-RapidAPI-Key": "536b801694mshccc77a40795ce48p1f3633jsn910ec7634824",
+  "X-RapidAPI-Key": "4ef8590048msh5350f4effeca8bcp195e32jsnb98bc462a0fa",
 };
 const baseUrl = "https://coinranking1.p.rapidapi.com";
 const createRequest = (url) => ({ url, headers: cryptoApiHeaders });
@@ -16,7 +16,7 @@ export const cryptoApi = createApi({
       query: (id) => createRequest(`/coin/${id}`),
     }),
     getExchanges: builder.query({
-      query: () => createRequest("/exchanges"),
+      query: () => createRequest(`/markets`),
     }),
     getCryptoHistory: builder.query({
       query: ({ coinId, timeperiod }) =>
@@ -28,4 +28,5 @@ export const {
   useGetCryptosQuery,
   useGetCryptoDetailsQuery,
   useGetCryptoHistoryQuery,
+  useGetExchangesQuery,
 } = cryptoApi;
